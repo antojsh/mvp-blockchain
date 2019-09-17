b75058885fead1b5edd1e37881eea231930daea7
Antonio Sierra <antoniojsh93@gmail.com>
2019-09-16 20:07:27 -0500

    Initial Page

---
+++ src/app/hash-generator-page/hash-generator-page.component.spec.ts
@@ -0,0 +1,27 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HashGeneratorPageComponent } from './hash-generator-page.component';
+
+describe('HashGeneratorPageComponent', () => {
+  let component: HashGeneratorPageComponent;
+  let fixture: ComponentFixture<HashGeneratorPageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HashGeneratorPageComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HashGeneratorPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/hash-generator-page/hash-generator-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hash-generator-page',
+  templateUrl: './hash-generator-page.component.html',
+  styleUrls: ['./hash-generator-page.component.scss'],
+})
+export class HashGeneratorPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
